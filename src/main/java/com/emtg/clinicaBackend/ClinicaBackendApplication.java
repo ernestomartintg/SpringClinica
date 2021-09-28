8acf70096732eb09f7ffb89133248b3eae9c78a4
ErnestoTorres <ernestomartintg@gmail.com>
2021-09-28 16:42:16 -0500

    Commit inicial

---
+++ src/main/java/com/emtg/clinicaBackend/ClinicaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emtg.clinicaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaBackendApplication.class, args);
+	}
+
+}
